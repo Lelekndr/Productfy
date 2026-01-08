5d521f2601083d8c5c7b913d4bb53bc054e97f86
Leandro Mendieta <leandro.mendieta13@gmail.com>
2026-01-08 09:29:19 -0400

    Initial Commit

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { ENV } from "./config/env";
+import { clerkMiddleware } from '@clerk/express';
+
+const app = express();
+app.use(cors({origin: ENV.FRONTEND_URL}))
+app.use(clerkMiddleware())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    message:"Welcome to Producting API - Powered by PostgreSQL, Drizzle ORM, and Clerk Auth!",
+    endpoints:{
+        users:"/api/users",
+        products:"/api/products",
+        comments:"/api/comments"
+    }
+  });
+});
+
+app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
